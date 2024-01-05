eb4d6ddf19fd9aefad51f42cdbe964d9170143fc
california444 <27780952+california444@users.noreply.github.com>
2024-01-05 10:49:46 +0100

    Delete settings.h

--- settings.h
+++
@@ -1,5 +0,0 @@
-// Spotify settings
-//String deviceName = "";
-String clientId = "825cf5ff4f534d0fae90224e722a88e6";
-String clientSecret = "f01f58184f9847f6938e6f86535d25d9";
-//String refreshToken = "";
